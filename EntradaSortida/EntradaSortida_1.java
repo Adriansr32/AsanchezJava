af3b97a0d068ceb9002927ce38a37ba062d33d7a
Sebasaurio . <sebasefp57@gmail.com>
2024-12-04 15:40:53 +0100

    Hola by Sebas

--- EntradaSortida/EntradaSortida_1.java
+++ EntradaSortida/EntradaSortida_1.java
@@ -4,13 +4,13 @@ public class EntradaSortida_1 {
  public static void main(String[] args) {
  Scanner in;
  in = new Scanner(System.in);
- System.out.println("Entra tres números enters:");
+ System.out.println("Entra tres numeros enters:");
  int a = in.nextInt();
  int b = in.nextInt();
  int c = in.nextInt();
  int suma = a + b + c;
  double resultat = suma / 2.0;
- System.out.printf("El resultat de la operació " + "(" + a + "+" + b + "+" + c + ")/2 és %.2f", resultat);
+ System.out.printf("El resultat de la operacio " + "(" + a + "+" + b + "+" + c + ")/2 és %.2f", resultat);
 in.close();
  }
  
