61f03a91dbd15a87a298520b16301dba69d4644d
adriansr23 <asanchezr@inscastellet.cat>
2025-06-12 16:45:51 +0200

    Update ArrayList_Examen.java

--- src/modular/Test/src/examen2_2/ArrayList_Examen.java
+++ src/modular/Test/src/examen2_2/ArrayList_Examen.java
@@ -1,68 +1,51 @@
-package examen2_2;
+package metodos;
 
 import java.util.*;
-/*
- * Un programa per demanar anar al lavabo i emmagatzemar-ho
- * 
- * @author Adrian Sanchez
- * @version 1.0
- * @since 07/02/2025
- */
-public class ArrayList_Examen {
-	/*
-	 * @param nom es vuit
-	 * @param Alumnes es l'array d'alumnes
-	 */
-	static ArrayList<String> Alumnes = new ArrayList<>();
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
 
-		int op = 0;
+public class Lavabo {
+	static ArrayList<String> wc = new ArrayList<>();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		boolean ter = false;
+		int op = 0;
 		while (!ter) {
 			menu();
-			
-			op = askOp(in, "Introdueix una opció: ");
+			op = askOp(sc);
+
 			switch (op) {
-			 case 1:
-	                System.out.println(AlumnesLlistad(Alumnes));
-	                break;
-	            case 2:
-	                System.out.println(ultimAlumne(Alumnes));
-	                break;
-	            case 3:
-	                String name = askIn(in, "Introdueix un nom: ");
-	                System.out.println("La alumne ha anat " + RecomptePerAlumne(Alumnes, name) + " vegades al lavabo");
-	                break;
-	            case 4:
-	                System.out.println(RecompteAlumnes(Alumnes));
-	                break;
-	            case 5:
-	                String name1 = askIn(in, "Introduex el teu nom: ");
-	                if (RecomptePerAlumne(Alumnes, name1) < 3) {
-	                	Solicitar(Alumnes, name1);
-	                	System.out.println("Pots entrar!");
-	                } else {
-	                	System.out.println("No pots anar mes de 3 vegadas, cagon!");
-	                }
-	                break;
-	            case 6:
-	                ter = Exit();
-	                break;
-	            default:
-	                System.out.println("opció no vàlida");
-	                break;
+			case 1:
+				System.out.println(queryUsage());
+				break;
+			case 2:
+				System.out.println(queryLastUsage());
+				break;
+			case 3:
+				System.out.println(queryUsageByStudent(askStudent(sc)));
+				break;
+			case 4:
+				System.out.println(queryTotalUsage());
+				break;
+			case 5:
+				if (askWc(askStudent(sc))) {
+					System.out.println("Puedes entrar!");
+				} else {
+					System.err.println("Ya has entrado pesao!");
+				}
+				break;
+			case 6:
+				ter = exit();
+				break;
+			default:
+				System.err.println("Opció no valida. Torna-ho provar.");
+				break;
 			}
+
 		}
-		
+
 	}
-	
-	/*
-	 * Fa el menu
-	 * 
-	 */
+
 	public static void menu() {
-		
 		System.out.println("Menu");
 		System.out.println("1. Consultar llista. ");
 		System.out.println("2. Consultar últim alumne ");
@@ -71,100 +54,104 @@ public class ArrayList_Examen {
 		System.out.println("5. Solicitar anar al lavabo ");
 		System.out.println("6. Exit");
 	}
-	
-	/*
-	 * Fa els alumnes llistats
-	 */
-	public static String AlumnesLlistad(ArrayList<String> alumnes) {
-	    StringBuilder result = new StringBuilder();
-	    for (int i = 0; i < alumnes.size() - 1; i++) {
-	        result.append(alumnes.get(i)).append(", ");
-	    }
-	    result.append(alumnes.get(alumnes.size() - 1)).append(".");
-	    return result.toString();
+
+	public static boolean askWc(String alumne) {
+		if (queryUsageByStudent(alumne) < 3) {
+			wc.add(alumne);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public static String ultimAlumne(ArrayList<String> alumnes) {
-	    int ultim = alumnes.size();
-	    return alumnes.get(ultim - 1);
+	public static String queryUsage() {
+		if (wc.isEmpty()) {
+			return "No hi ha alumnes";
+		}
+		StringBuilder rs = new StringBuilder();
+		for (int i = 0; i < wc.size() - 1; i++) {
+			rs.append(wc.get(i)).append(", ");
+		}
+		rs.append(wc.get(wc.size() - 1)).append(".");
+
+		return rs.toString();
 	}
 
-	public static int RecomptePerAlumne(ArrayList<String> alumnes, String name) {
-	    int contador = 0;
-	    for (String alumna : alumnes) {
-	        if (alumna.equals(name)) {
-	            contador++;
-	        }
-	    }
-	    return contador;
+	public static String queryLastUsage() {
+		if (wc.size() > 0) {
+			return wc.getLast();
+		} else {
+			return "";
+		}
 	}
 
-	public static String RecompteAlumnes(ArrayList<String> alumnes) {
-	    StringBuilder result = new StringBuilder();
-	    for (int i = 0; i < alumnes.size(); i++) {
-	        String alumne = alumnes.get(i);
-	        boolean jaComptat = false;
-
-	        for (int j = 0; j < i; j++) {
-	            jaComptat = jaComptat || alumnes.get(j).equals(alumne);
-	        }
-
-	        if (!jaComptat) {
-	            int contador = 0;
-	            for (int j = 0; j < alumnes.size(); j++) {
-	                if (alumnes.get(j).equals(alumne)) {
-	                    contador++;
-	                }
-	            }
-	            
-	            
-	            result.append(alumne).append(" -> ").append(contador).append("\n");
-	        }
-	    }
-	    return result.toString();
-	}
-
-	public static void Solicitar(ArrayList<String> alumnes, String name) {
-	        alumnes.add(name);
-	}
-	/*
-	 * Retorna un true si es exit
-	 */
-	public static boolean Exit() {
-		return true;
-	}
-	public static int askOp(Scanner in, String question) {
-		int num = 0;
-		boolean valid = false;
-		while (!valid) {
-			System.out.println(question);
-			System.out.print(">>> ");
-			if(in.hasNextInt()) {
-				num = in.nextInt();
-				valid = true;
-			} else {
-				System.err.println("Opció no valida. Torna-ho a provar");
-				in.next();
+	public static int queryUsageByStudent(String alumne) {
+		int count = 0;
+		for (String al : wc) {
+			if (al.equals(alumne)) {
+				count++;
 			}
 		}
-		return num;	
+		return count;
 	}
-	
-	public static String askIn(Scanner in, String question) {
-		 String paraula = null;
-	        boolean valid = false;
-	        while (!valid) {
-	            System.out.println(question); 
-	            System.out.print(">>> ");
-	            if (in.hasNextLine()) {
-	                paraula = in.nextLine();
-	                valid = true;  
-	            } else {
-	                System.err.println("Ha de ser un String!");
-	                in.next();  
-	            }
-	        }
-	        return paraula;  
-	    }
 
+	public static String queryTotalUsage() {
+		StringBuilder rs = new StringBuilder();
+		ArrayList<String> alreadySeen = new ArrayList<String>();
+		for (String student : wc) {
+			if (!alreadySeen.contains(student)) {
+				rs.append(student + " -> " + queryUsageByStudent(student) + ",\n");
+				alreadySeen.add(student);
+			}
+		}
+
+		return rs.toString();
+	}
+
+	public static int askOp(Scanner sc) {
+		int option = 0;
+		boolean valid = false;
+		System.out.println("Introdueix una opció: ");
+		System.out.print(">>");
+		while (!valid) {
+			option = askInt(sc);
+			if (option >= 1 && option <= 6) {
+				valid = true;
+			} else {
+				System.err.println("La opció ha d'estar entre 1 i 6");
+			}
+		}
+		return option;
+	}
+
+	public static int askInt(Scanner sc) {
+		int num = 0;
+		boolean valid = false;
+		System.out.println("Introdueix un número: ");
+		System.out.print(">>");
+		while (!valid) {
+			if (sc.hasNextInt()) {
+				num = sc.nextInt();
+				valid = true;
+			} else {
+				System.err.println("Opció no valida. Torna-ho a provar.");
+				sc.next();
+			}
+		}
+		return num;
+	}
+
+	public static String askString(Scanner sc) {
+		System.out.println("Introdueix un string: ");
+		return sc.nextLine();
+	}
+
+	public static String askStudent(Scanner sc) {
+		System.out.println("Introdueix el nom del estudiant: ");
+		return sc.nextLine();
+	}
+
+	public static boolean exit() {
+		return true;
+	}
 }
